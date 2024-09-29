d1b50045c90377fb29bcbdb729236fb3501347dc
Mahrez <mahreztrabelsi88@gmail.com>
2024-09-29 12:54:36 +0100

    fixing3

--- src/Server/Server.js
+++ src/Server/Server.js
@@ -1,45 +1,45 @@
-// server.js
-const express = require('express');
-const cors = require('cors');
-const app = express();
-app.use(cors());
-app.use(express.json());
+// // server.js
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
 
-let storedData = {};
-let userstoredData = {};
-
-
-// Sign Up APi Request
-
-app.post('/save-Signup', (req, res) => {
-    userstoredData = req.body;
-    res.json({ message: 'User Data saved successfully' });
-});
+// let storedData = {};
+// let userstoredData = {};
 
 
-app.get('/fetch-Signup', (req, res) => {
-    res.json(userstoredData);
-});
+// // Sign Up APi Request
+
+// app.post('/save-Signup', (req, res) => {
+//     userstoredData = req.body;
+//     res.json({ message: 'User Data saved successfully' });
+// });
 
 
-
-
-// Test Model APi Request
-app.post('/save-data', (req, res) => {
-    storedData = req.body;
-    res.json({ message: 'Data saved successfully' });
-});
-
-
-app.get('/fetch-data', (req, res) => {
-    res.json(storedData);
-});
+// app.get('/fetch-Signup', (req, res) => {
+//     res.json(userstoredData);
+// });
 
 
 
 
-const port = process.env.PORT || 8080;
+// // Test Model APi Request
+// app.post('/save-data', (req, res) => {
+//     storedData = req.body;
+//     res.json({ message: 'Data saved successfully' });
+// });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+
+// app.get('/fetch-data', (req, res) => {
+//     res.json(storedData);
+// });
+
+
+
+
+// const port = process.env.PORT || 8080;
+
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
